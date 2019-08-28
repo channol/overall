9157d8480b1cd2e222b5b6e4272328ba35da906d
lql <liangqinglong@casachina.com.cn>
2019-08-28 10:06:00 -0400

    98

--- allure-report.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /etc/profile
-/root/allure-2.12.1/bin/allure serve /root/overall/report -h 172.0.5.27 -p 35869
-
---
+++ get_allure.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /etc/profile
+/root/allure-2.12.1/bin/allure serve /root/overall/allure-report -h 172.0.5.27 -p 35869
+
