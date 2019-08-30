b95f6e3f15aee54693cb29521ab3cfbd18d18482
channol <424261385@qq.com>
2019-08-30 23:15:39 +0800

    Update 3smf-del.py

--- 3smf-del.py
+++ 3smf-del.py
@@ -4,6 +4,8 @@ import time,sys,os,re,logging
 from landslide import *
 from config import *
 from NF import *
+import allure
 
+@allure.issue('127')
 def test_smf_del_session():
     NF.smf_del_session("172.24.14.11",get_parameter('supi'),get_parameter('pdu_id'))
