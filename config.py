f3f2da08a03074dad84acb105a4c5deaf401ba31
lql <liangqinglong@casachina.com.cn>
2019-09-02 01:45:00 -0400

    up-cfg-1

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ dirpath = filepath+'/log/result'+strftime
 os.makedirs(dirpath)
 cap_file = dirpath+'/capture'+strftime+'.pcap'
 smfsm_file = dirpath+'/smfsm'+strftime+'.log'
+uemgr_file = dirpath+'/uemgr'+strftime+'.log'
 pfcp_file = dirpath+'/pfcp'+strftime+'.log'
 #print(cap_file,smfsm_file,pfcp_file)
 ###########################################
@@ -33,12 +34,18 @@ def get_log():
     os.chdir(path_log+'/dcomp/')
     print(cap_file)
     os.popen('sudo dcomp logs smfsm > {}'.format(smfsm_file))
+    os.popen('sudo dcomp logs uemgr > {}'.format(uemgr_file))
     os.popen('sudo dcomp logs pfcp > {}'.format(pfcp_file))
     if os.path.exists(smfsm_file):
         logging.info('scp root@172.0.5.27:'+smfsm_file+' .')
         logging.info('vim scp://root@172.0.5.27/'+smfsm_file)
     else:
         logging.warning('The sm log is not exist!')
+    if os.path.exists(uemgr_file):
+        logging.info('scp root@172.0.5.27:'+uemgr_file+' .')
+        logging.info('vim scp://root@172.0.5.27/'+uemgr_file)
+    else:
+        logging.warning('The uemgr log is not exist!')
     if os.path.exists(pfcp_file):
         logging.info('scp root@172.0.5.27:'+pfcp_file+' .')
         logging.info('vim scp://root@172.0.5.27/'+pfcp_file)
