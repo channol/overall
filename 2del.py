0244bbdb6cad3a5ea2d611e36295351e4cf75fc6
lql <liangqinglong@casachina.com.cn>
2019-09-02 20:40:07 -0400

    903

--- 2del.py
+++ 2del.py
@@ -7,6 +7,6 @@ from NF import *
 
 def test_landslide_case_delete():
     #ids = [293,295,291]
-    for idl in range(301,303):
+    for idl in range(340,360):
         Landslide.case_delete(str(idl))
 
