0c1cd741f03d12978c20184c68b80c3c1aa01014
lql <liangqinglong@casachina.com.cn>
2019-09-03 02:20:45 -0400

    ladf

--- test_003_smf_rel_pdu.py
+++ test_003_smf_rel_pdu.py
@@ -6,6 +6,7 @@ from config import *
 from NF import *
 
 #a = time.time()
+times = 5
 
 def test_003_start():
     capture_start()
@@ -31,9 +32,9 @@ def child_upf():
 
 #@pytest.fixture
 @pytest.fixture(scope = 'module')
-def ue_ip(get_test_id,smf_ip):
+def ue_ip(get_test_id,smf_ip,times):
     if get_test_id and smf_ip:
-        for i in range(10):
+        for i in range(times):
             logging.info('try get ue ip times: '+str(i))
             ue_ip = NF.smf_get_session(smf_ip,get_parameter('supi'),get_parameter('pdu_id'))
             if ue_ip:
@@ -47,6 +48,9 @@ def ue_ip(get_test_id,smf_ip):
 def test_003_landslide_case_start(get_test_id):
     assert get_test_id, "landslide is running a case!"
 
+def test_003_smf_ip(smf_ip):
+    assert smf_ip, "can not get smf ip!"
+
 def test_003_ue_ip(ue_ip):
     assert ue_ip, "can not get ue ip!"
 
@@ -57,12 +61,19 @@ def test_003_upf_session_rule(child_upf,ue_ip):
             NF.upf_session_rule(child_upf,ue_ip,rule)
     NF.upf_close(child_upf)
 
+def test_003_landslide_case_measurements_1(get_test_id):
+    if get_test_id:
+        logging.info('post measurements of landslide case')
+        ls = Landslide.case_measurements(get_test_id)
+    else:
+        return False
+
 def test_003_smf_del_session(smf_ip):
     NF.smf_del_session(smf_ip,get_parameter('supi'),get_parameter('pdu_id'))
 
-def test_003_landslide_case_continue(get_test_id):
+def test_003_landslide_case_continue(get_test_id,times):
     if get_test_id:
-        for i in range(10):
+        for i in range(times):
             logging.info('try continue landslide case times: '+str(i))
             ls = Landslide.case_state(get_test_id)
             if ls == '5_Waiting':
@@ -75,9 +86,24 @@ def test_003_landslide_case_continue(get_test_id):
     else:
         return False
 
-def test_003_landslide_case_delete(get_test_id):
+def test_003_landslide_case_measurements_2(get_test_id,times):
     if get_test_id:
-        for i in range(10):
+        for i in range(times):
+            logging.info('post measurements of landslide case times: '+str(i))
+            ls = Landslide.case_state(get_test_id)
+            if ls == 'COMPLETE' or ls == 'COMPLETE_ERROR':
+                ls = Landslide.case_measurements(get_test_id)
+                if ls:
+                    break
+                time.sleep(10)
+            else:
+                time.sleep(10)
+    else:
+        return False
+
+def test_003_landslide_case_delete(get_test_id,times):
+    if get_test_id:
+        for i in range(times):
             logging.info('try delete landslide case times: '+str(i))
             ls = Landslide.case_state(get_test_id)
             if ls == 'COMPLETE' or ls == 'COMPLETE_ERROR':
